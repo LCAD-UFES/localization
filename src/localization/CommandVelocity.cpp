e3dfde9686476137b56cc1cace80ec7a1c7c03e4
Josias Alexandre <josiasalexandre@gmail.com>
2015-10-09 18:30:03 -0300

    Podemos incluir os algoritmos nas classes adequadas

--- src/localization/CommandVelocity.cpp
+++ src/localization/CommandVelocity.cpp
@@ -3,9 +3,6 @@
 // basic constructor
 CommandVel::CommandVel(const geometry_msgs::TwistStamped &msg) : CommandReader(), cmd() {
     cmd = msg;
-    std::cout << "SEQ: " << msg.header.seq << std::endl;
-    std::cout << "TIME: " << msg.header.stamp << std::endl;
-    std::cout << "Frame ID: " << msg.header.frame_id << std::endl;
 }
 
 // basic constructor
@@ -17,7 +14,4 @@ CommandVel::CommandVel(const geometry_msgs::Twist &msg, unsigned int seq) : Comm
     cmd.header.seq = seq;
     cmd.header.stamp = ros::Time::now(); // not so good... :-(
     cmd.header.frame_id = "/base_link";
-    std::cout << "SEQ: " << cmd.header.seq << std::endl;
-    std::cout << "TIME: " << cmd.header.stamp << std::endl;
-    std::cout << "Frame ID: " << cmd.header.frame_id << std::endl;
 }
