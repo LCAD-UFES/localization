24e9ce39a7edf7c92f32c92cd68cdfa28029b485
Vinicius <viniciusbc1@gmail.com>
2015-10-26 17:57:21 -0200

    Modificando os z_hit,short...

--- src/localization/BeamRangeFinderModel.cpp
+++ src/localization/BeamRangeFinderModel.cpp
@@ -6,17 +6,17 @@
 BeamRangeFinderModel::BeamRangeFinderModel(ros::NodeHandle &private_nh, Laser *ls, Map *m) : MeasurementModel(ls, m), laser_update(), map_update() {
 
     // get the z_hit parameter
-    private_nh.param("likelihood_z_hit", z_hit, 0.7);
+    private_nh.param("beam_z_hit", z_hit, 0.7);
     // get the z_short parameter
-    private_nh.param("likelihood_z_short", z_short, 0.2);//
+    private_nh.param("beam_z_short", z_short, 0.2);//
     // get the z_max parameter
-    private_nh.param("likelihood_z_max", z_max, 0.05);
+    private_nh.param("beam_z_max", z_max, 0.05);
     // get the z_rand parameter
-    private_nh.param("likelihood_z_rand", z_rand, 0.05);
+    private_nh.param("beam_z_rand", z_rand, 0.05);
     // get the sigma_hit parameter
-    private_nh.param("likelihood_sigma_hit", sigma_hit, 0.2);
+    private_nh.param("beam_sigma_hit", sigma_hit, 0.2);
     // get the lambda short
-    private_nh.param("likelihood_lambda_short", lambda_short, 0.2);
+    private_nh.param("beam_lambda_short", lambda_short, 0.2);
 
     // get the max_beams parameter, see MeasurementModel base class
     private_nh.param("laser_max_beams", max_beams, 60);
